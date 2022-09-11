2646ab1c336792a7b1c89d86bd0f55804507fe3e
Angelica Flutur <94394449+angiflutur@users.noreply.github.com>
2022-09-11 15:06:29 +0300

    Delete Programarea Algoritmilor/Laborator directory

--- Programarea Algoritmilor/Laborator/laborator 3 - solutii/141 gr 1 codingrooms/1_operatii_liste.py
+++
@@ -1,7 +0,0 @@
-"""
-1.	Se dau două liste l1 si l2 de lungime n. Să se înlocuiască elementele de pe poziții (numerotate de la 1) pare din l1 cu cele de pe poziția corespunzătoare din l2 folosind feliere (slice) 
-"""
-l1 = [int(x) for x in input().split()] #l1=[1,2,5,6]
-l2 = [int(x) for x in input().split()] #l2=[8,12,11,15] =>l1=[1,12,5,15]
-l1[1::2]=l2[1::2]
-print(l1)
