41e2e5925a7076124c91e5d155fc1d08de7ff992
Angelica Flutur <94394449+angiflutur@users.noreply.github.com>
2023-04-02 20:29:51 +0300

    Add files via upload

---
+++ Anul 2/Semestrul 1/Dezvoltarea Aplicatiilor Web (ASP .NET)/Lab1_13/Lab1_13/Lab1_13/Lab1_13/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_13
+{
+    public class Class1 : Interface1
+    {
+        public string CompleteName
+        {
+            get { return FirstName + " " + LastName; }
+            set { CompleteName = value; }
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Description1 { get; set; }
+        protected internal string Description2 { get; set; }
+        protected string Description3 { get; set; }
+
+        internal string Description4 { get; set; }
+        private protected string Description5 { get; set;}
+
+        private  string Description6 { get; set; }
+
+        [Obsolete("Please use Method 2")]
+        public void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+}
---
+++ Anul 2/Semestrul 1/Dezvoltarea Aplicatiilor Web (ASP .NET)/Lab1_13/Lab1_13/Lab1_13/Lab1_13/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//var a = new object[] { 1, 2, 3 };
+//var b = new object[] { "a", "b", "c" };
+
+//for (var i = 0; i < a.Length; i++)
+//{
+//   Console.WriteLine(a[i]);
+//}
+
+//foreach (string s in b)
+//{
+//    // Console.WriteLine(s);
+//}
+
+//dynamic student = new { LastName = "Student Lastname 1", Grupa = "13" };
+////Console.WriteLine(student.LastName);
+//try
+//{
+//    // student.FirstName = "FirstName 1";
+//}
+//catch (Exception e)
+//{
+//    // Console.WriteLine(e.ToString());
+//}
+
+
+//const int number = 9;
+//// number = 10;
+
+//// Console.ReadLine();
+
+//List<int> numbers = new List<int>();
+//numbers.Add(number);
+//numbers.Add(10);
+//numbers.Add(20);
+
+////Console.WriteLine(numbers.Where(x => x > 10).FirstOrDefault());
+////Console.WriteLine(numbers.FirstOrDefault(x => x < 10));
+
+//int sum(int a, int b, int c = 10)
+//{
+//    return a + b + c;
+//}
+
+
+//Console.WriteLine(sum(1, 2));
+////Console.WriteLine(sum(1, 2, 3));
+
+//// ref 
+//void sumWithRef(int a, int b, ref int c)
+//{
+//    c = a + b;
+//}
+
+//int num = 2;
+//sumWithRef(4, 5, ref num);
+////Console.WriteLine(num);
+
+
+//// out
+//void sumWithOut(int a, int b, out int c)
+//{
+//    c = a + b;
+//}
+
+//int c;
+//sumWithOut(4, 4, out c);
+////Console.WriteLine(c);
+
+using Lab1_13;
+using Newtonsoft.Json;
+///
+var class1Obj = new Class1();
+class1Obj.FirstName = "First Name 1";
+class1Obj.LastName = "Last Name 1";
+
+//Console.WriteLine(class1Obj.CompleteName);
+//Console.WriteLine(class1Obj.ToString());
+string objectSerialized = JsonConvert.SerializeObject(class1Obj);
+
+Console.WriteLine(objectSerialized);
+
+class1Obj.Method1();
